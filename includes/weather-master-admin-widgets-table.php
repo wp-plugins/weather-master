4eb239ca1f54d6f63c2905ad3743d5c773d7db54
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-30 23:33:48 +0000

    version 4.4.1.4
    git-svn-id: https://plugins.svn.wordpress.org/weather-master/tags/4.4.1.4@1123966 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/weather-master-admin-widgets-table.php
@@ -0,0 +1,56 @@
+<?php
+if(!class_exists('WP_List_Table')){
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
+class weather_master_admin_widgets_table extends WP_List_Table {
+	/**
+	 * Display the rows of records in the table
+	 * @return string, echo the markup of the rows
+	 */
+	function display() {
+?>
+<table class="widefat fixed" cellspacing="0">
+	<thead>
+		<tr>
+			<th id="columnname" class="manage-column column-columnname" scope="col" width="300"><legend><h3><img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" /><?php _e('&nbsp;Screenshot', 'weather_master'); ?></h3></legend></th>
+			<th id="columnname" class="manage-column column-columnname" scope="col"><h3><img src="<?php echo plugins_url('../images/techgasp-minilogo-16.png', __FILE__); ?>" style="float:left; height:16px; vertical-align:middle;" /><?php _e('&nbsp;Description', 'weather_master'); ?></h3></legend></th>
+		</tr>
+	</thead>
+
+	<tfoot>
+		<tr>
+			<th class="manage-column column-columnname" scope="col" width="300"><a class="button-primary" href="<?php echo get_site_url(); ?>/wp-admin/widgets.php" title="To Widgets Page" style="float:left;">To Widgets Page</a></p></th>
+			<th class="manage-column column-columnname" scope="col"><a class="button-primary" href="<?php echo get_site_url(); ?>/wp-admin/widgets.php" title="To Widgets Page" style="float:right;">To Widgets Page</a></p></th>
+		</tr>
+	</tfoot>
+
+	<tbody>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-weathermaster-admin-widget-basic.png', __FILE__); ?>" alt="<?php echo get_option('weather_master_name'); ?>" align="left" width="300px" height="135px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Weather Master Basic Widget</h3><p>Fast Loading and easy to deploy Widget specially designed in html5 for extremely fast page load times and small system trace. Weather Master Basic Widget uses the latest weather forecast information at a city level detail and provides excellent Google SEO.</p><p>Navigate to your wordpress widgets page and start using it.</p></td>
+		</tr>
+		<tr>
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-weathermaster-admin-widget-advanced.png', __FILE__); ?>" alt="<?php echo get_option('weather_master_name'); ?>" align="left" width="300px" height="135px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Weather Master Advanced Widget</h3><p>This is the "top of the line" business news type of weather for Wordpress. <b>Fully Mobile Responsive</b>, this widget was specially designed to grant you full control over all weather options, easily customizable.</p><p>Check Add-ons page.</p></td>
+		</tr>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-weathermaster-admin-widget-current.png', __FILE__); ?>" alt="<?php echo get_option('weather_master_name'); ?>" align="left" width="300px" height="135px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Weather Master Current Weather Widget</h3><p>Displays weather for any location on Earth including over 200,000 cities. Easy to use, fast loading and packed with gorgeous Android HTC Weather Icons.</p><p>Check Add-ons page.</p></td>
+		</tr>
+		<tr>
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-weathermaster-admin-widget-forecast.png', __FILE__); ?>" alt="<?php echo get_option('weather_master_name'); ?>" align="left" width="300px" height="135px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Weather Master Forecast Widget</h3><p>Provides 7 days of weather forecast from over 200,000 cities. Packed with gorgeous Android HTC Weather Icons.</p><p>Check Add-ons page.</p></td>
+		</tr>
+		<tr class="alternate">
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-weathermaster-admin-widget-dashboard.png', __FILE__); ?>" alt="<?php echo get_option('weather_master_name'); ?>" align="left" width="300px" height="135px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Weather Master Administrator Dashboard Widget</h3><p>Cool Administrator Widget to keep track of the weather outside while you work on your wordpress. Small system trace.</p><p>Check Add-ons page.</p></td>
+		</tr>
+		<tr>
+			<td class="column-columnname" width="300" style="vertical-align:middle"><img src="<?php echo plugins_url('../images/techgasp-admin-widget-blank.png', __FILE__); ?>" alt="<?php echo get_option('amazon_master_name'); ?>" align="left" width="300px" height="135px" style="padding:5px;"/></td>
+			<td class="column-columnname"style="vertical-align:middle"><h3>Suggest a Widget</h3><p>Would you like to see your widget idea added to this plugin? Just drop us a line and we will make sure it gets included in the next release.</p><p>Get in touch with TechGasp.</p></td>
+		</tr>
+	</tbody>
+</table>
+<?php
+		}
+}
